39a8eed5f507e1e82f533be2bd1a01c73deb7fd6
Ricky White <ricky@whitelionmedia.com>
2022-01-22 22:19:04 -0500

    Updated

--- Add-Tool.ps1
+++ Add-Tool.ps1
@@ -1,29 +1,51 @@
 ﻿[CmdletBinding()]
 param (
     [Parameter(Mandatory, Position = 0)]
-    [Path]
-    $ZipFilePath,
+    [string]
+    $ZipFile,
 
     [Parameter(Mandatory, Position = 1)]
-    [Path]
-    $OutputFolderPath,
-
-    [Parameter()]
     [string]
-    $FolderName
+    $OutputFolder
 )
 
-Get-Item $ZipFilePath | Expand-Archive -DestinationPath $OutputFolderPath
+$File = Get-Item -Path $ZipFile
 
-# Add to User PATH
+
+if (-not $File.Extension -eq ".zip")
+{
+    Write-Error "$Zipfile is not a zip file" -ErrorAction Stop
+}
+
+# check if the unzip folder already exists in output folder
+$UnzippedFolderName = $File.Name.Split('.')[0]
+
+if (Test-Path -Path "$OutputFolder\$UnzippedFolderName")
+{
+    Write-Error "$UnzippedFolderName already exists in $OutputFolder" -ErrorAction Stop
+}
+
 $UserPATH = (Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name PATH).path
 
-$UpdatedPATH = "$UserPATH;$OutputFolderPath"
+if ($UserPATH.Contains($OutputFolder))
+{
+    Write-Output "PATH already contains $OutputFolder" -ErrorAction Stop
+}
 
-Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name PATH -Value $UpdatedPATH
+try {
+    $File | Expand-Archive -DestinationPath $OutputFolder
+    $NewPATH = $UserPATH + ";" + $OutputFolder
+    Set-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name PATH -Value $NewPATH
+}
+catch {
+    Write-Error "Failed to set PATH" -ErrorAction Stop
+}
 
-# TODO
-# Add support to rename folder of the unzipped folder
-# Check the file is a valid zip file
-# Check that the folder isn't already in the path
-# Check path to ensure it has been added correctly
+$CheckPATH = (Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name PATH).path
+
+if (-not $CheckPATH.Contains($OutputFolder))
+{
+    Write-Error "Failed to set PATH" -ErrorAction Stop
+}
+
+Write-Host "Successfully added $OutputFolder to PATH"
